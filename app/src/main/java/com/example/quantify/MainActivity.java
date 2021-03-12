f35b47597185139eac5084276b9e10e062a601c5
mimjahratul <mimjahratul@gmail.com>
2021-03-11 19:31:03 -0700

    popup menu done

--- app/src/main/java/com/example/quantify/MainActivity.java
+++ app/src/main/java/com/example/quantify/MainActivity.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        FloatingActionButton fab;
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, fab);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();//showing popup menu
+            }
+        });//closing the setOnClickListener method
     }
-
-
 }
