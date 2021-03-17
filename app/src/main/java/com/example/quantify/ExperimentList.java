a4abacbb6ca95ca8c7a72c9cae859dcd93e88eda
NahinChowdhury <46066736+NahinChowdhury@users.noreply.github.com>
2021-03-17 23:05:00 +0600

    Fixed merge issues

--- app/src/main/java/com/example/quantify/ExperimentList.java
+++ app/src/main/java/com/example/quantify/ExperimentList.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,7 +31,7 @@ public class ExperimentList extends ArrayAdapter<Experiment> {
         View view = convertView;
 
         if (view == null){
-            view = LayoutInflater.from(context).inflate(R.layout.card, parent, false);
+            view = LayoutInflater.from(context).inflate(R.layout.owner_card, parent, false);
         }
 
         Experiment experiment = experiments.get(position);
