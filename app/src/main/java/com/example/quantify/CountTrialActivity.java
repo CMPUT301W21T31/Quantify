b597b6ebcf80697566840e0b9e03620043daa6c2
Ferdous Ahmed Adit <ferdousadit99@gmail.com>
2021-03-19 13:24:44 -0600

    Can Log into questions from any experiment

--- app/src/main/java/com/example/quantify/CountTrialActivity.java
+++ app/src/main/java/com/example/quantify/CountTrialActivity.java
@@ -2,7 +2,9 @@ package com.example.quantify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CountTrialActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CountTrialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.count_trial);
+
+
+    }
+
+    public void OpenQuestionList(View view) {
+        Intent intent = new Intent(this, Question.class);
+        startActivity(intent);
     }
 }
