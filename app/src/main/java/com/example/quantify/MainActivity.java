f8949b48085a2b3e56a6374784df5f07fb390a2c
NahinChowdhury <46066736+NahinChowdhury@users.noreply.github.com>
2021-04-02 05:36:26 +0600

    Fixed merges

--- app/src/main/java/com/example/quantify/MainActivity.java
+++ app/src/main/java/com/example/quantify/MainActivity.java
@@ -25,10 +25,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
@@ -41,18 +37,8 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -82,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         floatingActionButton = findViewById(R.id.floatingActionButton);
@@ -118,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
                 experimenterExperimentDataList.clear();
                 ownerExperimentDataList.clear();
-                for(QueryDocumentSnapshot doc: queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
                     Log.d("TAG", String.valueOf(doc.getData().get("Province Name")));
                     String experiment_description = doc.getId();
                     String experiment_username = (String) doc.getData().get("Experiment User");
@@ -132,8 +117,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
 
 
 //        FloatingActionButton fab;
@@ -155,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.search:
                         // Handle search icon press
                         String id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
@@ -182,15 +165,14 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Toast.makeText(MainActivity.this,"You Clicked : " + tab.getPosition(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You Clicked : " + tab.getPosition(), Toast.LENGTH_SHORT).show();
 
-                if( tab.getPosition() == 0){
+                if (tab.getPosition() == 0) {
                     floatingActionButton.setVisibility(View.VISIBLE);
                     tabPos = 0;
                     experimentList.setAdapter(ownerExperimentAdapter);
                     ownerExperimentAdapter.notifyDataSetChanged();
-                }
-                else{
+                } else {
                     floatingActionButton.setVisibility(View.INVISIBLE);
                     tabPos = 1;
                     experimentList.setAdapter(experimenterExperimentAdapter);
@@ -213,7 +195,7 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab;
         fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         FloatingActionButton finalFab1 = fab;
-        fab.setOnClickListener(new View.OnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -225,62 +207,60 @@ public class MainActivity extends AppCompatActivity {
                 //registering popup with OnMenuItemClickListener
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
                         //Log.d("BLABLA",(String) item.getTitle());
 
-                         if(((String) item.getTitle()).equals("Add New")){
-                             View view_1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_experiment_fragment_layout, null);
-                             expDesc = view_1.findViewById(R.id.exp_desc_fragment);
-                             expUser = view_1.findViewById(R.id.exp_user_fragment);
-                             expStatus = view_1.findViewById(R.id.exp_status_fragment);
-                             expType = view_1.findViewById(R.id.exp_type_fragment);
+                        if (((String) item.getTitle()).equals("Add New")) {
+                            View view_1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_experiment_fragment_layout, null);
+                            expDesc = view_1.findViewById(R.id.exp_desc_fragment);
+                            expUser = view_1.findViewById(R.id.exp_user_fragment);
+                            expStatus = view_1.findViewById(R.id.exp_status_fragment);
+                            expType = view_1.findViewById(R.id.exp_type_fragment);
 
 
-                             AlertDialog.Builder adb=new AlertDialog.Builder(MainActivity.this);
-                             adb.setTitle("Add?");
-                             adb.setMessage("Are you sure you want to Add Experiment");
-                             adb.setView(view_1);
-                             adb.setNegativeButton("Cancel", null);
-                             adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
-                                 public void onClick(DialogInterface dialog, int which) {
-                                     String exp_description = expDesc.getText().toString();
-                                     String exp_username = expUser.getText().toString();
-                                     String exp_status = expStatus.getText().toString();
-                                     String exp_type = expType.getText().toString();
+                            AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                            adb.setTitle("Add?");
+                            adb.setMessage("Are you sure you want to Add Experiment");
+                            adb.setView(view_1);
+                            adb.setNegativeButton("Cancel", null);
+                            adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String exp_description = expDesc.getText().toString();
+                                    String exp_username = expUser.getText().toString();
+                                    String exp_status = expStatus.getText().toString();
+                                    String exp_type = expType.getText().toString();
 
 //  FIREBASE STUFF BEGINS
-                                     HashMap<String, String> data = new HashMap<>();
-                                     if (exp_description.length()>0 && exp_username.length()>0) {
-                                            data.put("Experiment User", exp_username);
-                                            data.put("Experiment Status", exp_status);
-                                            data.put("Experiment Type", exp_type);
-                                     }
+                                    HashMap<String, String> data = new HashMap<>();
+                                    if (exp_description.length() > 0 && exp_username.length() > 0) {
+                                        data.put("Experiment User", exp_username);
+                                        data.put("Experiment Status", exp_status);
+                                        data.put("Experiment Type", exp_type);
+                                    }
 
-                                     collectionReference
-                                             .document(exp_description)
-                                             .set(data)
-                                             .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                 @Override
-                                                 public void onSuccess(Void aVoid) {
+                                    collectionReference
+                                            .document(exp_description)
+                                            .set(data)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
                                                     // These are a method which gets executed when the task is succeeded
-                                                     Log.d("TAG", "Data has been added successfully!");
-                                                 }
-                                             })
-                                             .addOnFailureListener(new OnFailureListener() {
-                                                 @Override
-                                                 public void onFailure(@NonNull Exception e) {
-                                                        // These are a method which gets executed if there’s any problem
-                                                     Log.d("TAG", "Data could not be added!" + e.toString());
-                                                 }
-                                             });
-
-
+                                                    Log.d("TAG", "Data has been added successfully!");
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    // These are a method which gets executed if there’s any problem
+                                                    Log.d("TAG", "Data could not be added!" + e.toString());
+                                                }
+                                            });
 
 
 //  FIREBASE STUFF ENDS
 
-                                     ownerExperimentDataList.add(new Experiment(exp_description, exp_username, exp_status, exp_type));
-                                     experimenterExperimentDataList.add(new Experiment(exp_description, exp_username, exp_status, exp_type));
+                                    ownerExperimentDataList.add(new Experiment(exp_description, exp_username, exp_status, exp_type));
+                                    experimenterExperimentDataList.add(new Experiment(exp_description, exp_username, exp_status, exp_type));
 
 //                                     current_exp.setExp_desc(exp_description);
 //                                     current_exp.setUser(exp_username);
@@ -292,19 +272,19 @@ public class MainActivity extends AppCompatActivity {
 //                        Experiment mycity = new Experiment(exp_name, exp_description, null,S_Total,F_Total);
 //                        experimentAdapter.remove(experimentAdapter.getItem(positionToRemove));
 //                        experimentAdapter.insert(mycity, positionToRemove);
-                                     ownerExperimentAdapter.notifyDataSetChanged();
+                                    ownerExperimentAdapter.notifyDataSetChanged();
 
 
-                                 }});
-                             adb.show();
+                                }
+                            });
+                            adb.show();
 
-                         }
-                         else if(((String) item.getTitle()).equals("Subscribed")){
-                             Log.d("BLABLA",subscribedExperimentDataList.toString());
-                             Intent intent = new Intent(MainActivity.this, SubscribedActivity.class);
-                             intent.putExtra("subscribed",subscribedExperimentDataList);
-                             startActivity(intent);
-                         }
+                        } else if (((String) item.getTitle()).equals("Subscribed")) {
+                            Log.d("BLABLA", subscribedExperimentDataList.toString());
+                            Intent intent = new Intent(MainActivity.this, SubscribedActivity.class);
+                            intent.putExtra("subscribed", subscribedExperimentDataList);
+                            startActivity(intent);
+                        }
 
 
                         return true;
@@ -316,32 +296,29 @@ public class MainActivity extends AppCompatActivity {
         });//closing the setOnClickListener method
 
 
-        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 String experiment_type = (String) experimenterExperimentAdapter.getItem(position).getType();
 
-                if (experiment_type.equals("Binomial")){
-                    Log.d("BLABLA","Binomial Clicked");
+                if (experiment_type.equals("Binomial")) {
+                    Log.d("BLABLA", "Binomial Clicked");
                     Intent intent_1 = new Intent(MainActivity.this, BinomialTrialActivity.class);
-                    intent_1.putExtra("typename",experimenterExperimentAdapter.getItem(position));
+                    intent_1.putExtra("typename", experimenterExperimentAdapter.getItem(position));
                     startActivity(intent_1);
-                }
-                else if(experiment_type.equals("Count")){
-                    Log.d("BLABLA","Count Clicked");
+                } else if (experiment_type.equals("Count")) {
+                    Log.d("BLABLA", "Count Clicked");
                     Intent intent_1 = new Intent(MainActivity.this, CountTrialActivity.class);
-                    intent_1.putExtra("typename",experimenterExperimentAdapter.getItem(position));
+                    intent_1.putExtra("typename", experimenterExperimentAdapter.getItem(position));
                     startActivity(intent_1);
-                }
-                else if(experiment_type.equals("Temperature")){
-                    Log.d("BLABLA","Temperature clicked");
+                } else if (experiment_type.equals("Temperature")) {
+                    Log.d("BLABLA", "Temperature clicked");
                     Intent intent_1 = new Intent(MainActivity.this, MeasurementTrialActivity.class);
-                    intent_1.putExtra("typename",experimenterExperimentAdapter.getItem(position));
+                    intent_1.putExtra("typename", experimenterExperimentAdapter.getItem(position));
                     startActivity(intent_1);
-                }
-                else if(experiment_type.equals("Non-neg")){
-                    Log.d("BLABLA","Non-neg clicked");
+                } else if (experiment_type.equals("Non-neg")) {
+                    Log.d("BLABLA", "Non-neg clicked");
                     Intent intent_1 = new Intent(MainActivity.this, NonNegativeCountTrialActivity.class);
-                    intent_1.putExtra("typename",experimenterExperimentAdapter.getItem(position));
+                    intent_1.putExtra("typename", experimenterExperimentAdapter.getItem(position));
                     startActivity(intent_1);
                 }
             }
@@ -360,7 +337,7 @@ public class MainActivity extends AppCompatActivity {
         bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.location:
                         // Handle location icon press
                         Intent intent_2 = new Intent(MainActivity.this, MapsActivity.class);
@@ -371,36 +348,9 @@ public class MainActivity extends AppCompatActivity {
 
                     case R.id.qr_code:
                         // Handle qr_code icon press
-
-
-        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-        FloatingActionButton finalFab = fab;
-        fab.setOnClickListener(new View.OnClickListener(){
-
                 }
                 return false;
             }
         });
-    }
-
-
-            @Override
-            public void onClick(View v) {
-                //Creating the instance of PopupMenu
-                PopupMenu popup = new PopupMenu(MainActivity.this, finalFab);
-                //Inflating the Popup using xml file
-                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
-
-                //registering popup with OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                        return true;
-                    }
-                });
-
-                popup.show();//showing popup menu
-            }
-        });//closing the setOnClickListener method
     }
 }
