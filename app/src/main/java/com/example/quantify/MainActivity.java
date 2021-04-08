235fc90a18aaee0c261c206456d43cbb745c223c
Halo103109 <Halo103109@gmail.com>
2021-04-07 20:47:39 -0600

    finished QR code scanning

--- app/src/main/java/com/example/quantify/MainActivity.java
+++ app/src/main/java/com/example/quantify/MainActivity.java
@@ -4,7 +4,6 @@ package com.example.quantify;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -37,6 +36,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -386,10 +388,60 @@ public class MainActivity extends AppCompatActivity {
 
                     case R.id.qr_code:
                         // Handle qr_code icon press
+                        IntentIntegrator intentIntegrator = new IntentIntegrator(
+                                MainActivity.this
+                        );
+                        //set prompt text
+                        intentIntegrator.setPrompt("For flash use volume up key");
+                        //set beep
+                        intentIntegrator.setBeepEnabled(true);
+                        //locked orientation
+                        intentIntegrator.setOrientationLocked(true);
+                        //set capture activity
+                        intentIntegrator.setCaptureActivity(Capture.class);
+                        //initiate scan
+                        intentIntegrator.initiateScan();
                 }
                 return false;
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //initialize intent result
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(
+                requestCode,resultCode,data
+        );
+        //check condition
+        if (intentResult.getContents() != null) {
+            //when result content is not null
+            //initialize alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    MainActivity.this
+            );
+            //set title
+            builder.setTitle("Result");
+            //set message
+            builder.setMessage(intentResult.getContents());
+            //set positive button
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    //dismiss dialog
+                    dialogInterface.dismiss();
+                }
+            });
+            //show alert dialog
+            builder.show();
+        }else {
+            //when result content is null
+            //display toast
+            Toast.makeText(getApplicationContext()
+            ,"OOPS... You did not scan anything", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
